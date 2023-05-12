200dfb70eaf1556e804224d36d5edb5a6aad6a69
FerCasco <fernandocc@iescastelar.com>
2023-05-12 10:47:12 +0200

    migracion tipos

--- resources/views/livewire/gestionar-almacen.blade.php
+++ resources/views/livewire/gestionar-almacen.blade.php
@@ -1,7 +1,7 @@
 <center>
 <h1 class="mt-24 w-full text-center mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white inline-block">Tipo: {{$tipo->nombre}}</h1>
 
-<div class="grid grid-cols-3 gap-4 ml-52 mt-24 pl-28">
+<div class="grid grid-cols-3 gap-4 ml-52 mt-12 pl-28">
     @foreach($mercancias as $mercancia)
         <a class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$mercancia->nombre}}</h5>
