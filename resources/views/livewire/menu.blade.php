f8349c5bf13bdabc1717ffe09ff7a66c02d91c48
unknown <ruben@gaeinformatica.com>
2023-05-17 12:04:36 +0200

    Tamaño iconos

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -72,29 +72,29 @@
     <div class="fixed my-4 flex bg-gray-100 hover:bg-gray-200 rounded-lg transition-all p-1 dark:bg-gray-700 dark:hover:bg-gray-600">
         <nav class="flex space-x-2">
             <a class="group py-3 px-4 inline-flex flex-col items-center gap-2 hover:bg-white hover:scale-105 transition-all text-sm text-gray-700 font-medium rounded-md shadow-lg dark:bg-gray-800 dark:text-gray-400 w-32 justify-center" href="/">
-                <ion-icon name="home" class="-translate-y-20 transition-all group-hover:translate-y-0"></ion-icon>
+                <ion-icon name="home" class="-translate-y-20 transition-all group-hover:translate-y-0 text-2xl"></ion-icon>
                 <p class="-translate-y-2 group-hover:translate-y-0">Home</p>
             </a>
             <a class="group py-3 px-4 inline-flex flex-col items-center gap-2 hover:bg-white hover:scale-105 transition-all text-sm text-gray-700 font-medium rounded-md shadow-lg dark:bg-gray-800 dark:text-gray-400 w-32 justify-center" href="/cocina">
-                <ion-icon name="restaurant"  class="-translate-y-20 transition-all group-hover:translate-y-0"></ion-icon>
+                <ion-icon name="restaurant"  class="-translate-y-20 transition-all group-hover:translate-y-0 text-2xl"></ion-icon>
                 <p class="-translate-y-2 group-hover:translate-y-0">Cocina</p>
 
             </a>
             <a class="group py-3 px-4 inline-flex flex-col items-center gap-2 hover:bg-white hover:scale-105 transition-all text-sm text-gray-700 font-medium rounded-md shadow-lg dark:bg-gray-800 dark:text-gray-400 w-32 justify-center" href="/cocina">
-                <ion-icon name="cube"  class="-translate-y-20 transition-all group-hover:translate-y-0"></ion-icon>
+                <ion-icon name="cube"  class="-translate-y-20 transition-all group-hover:translate-y-0 text-2xl"></ion-icon>
                 <p class="-translate-y-2 group-hover:translate-y-0">Inventario</p>
 
             </a>
             <a class="group py-3 px-4 inline-flex flex-col items-center gap-2 hover:bg-white hover:scale-105 transition-all text-sm text-gray-700 font-medium rounded-md shadow-lg dark:bg-gray-800 dark:text-gray-400 w-32 justify-center" href="/contactos">
-                <ion-icon name="mail-unread"  class="-translate-y-20 transition-all group-hover:translate-y-0"></ion-icon>
+                <ion-icon name="mail-unread"  class="-translate-y-20 transition-all group-hover:translate-y-0 text-2xl"></ion-icon>
                 <p class="-translate-y-2 group-hover:translate-y-0">Contactos</p>
             </a>
             <a class="group py-3 px-4 inline-flex flex-col items-center gap-2 hover:bg-white hover:scale-105 transition-all text-sm text-gray-700 font-medium rounded-md shadow-lg dark:bg-gray-800 dark:text-gray-400 w-32 justify-center" href="/graficas">
-                <ion-icon name="bar-chart"  class="-translate-y-20 transition-all group-hover:translate-y-0"></ion-icon>
+                <ion-icon name="bar-chart"  class="-translate-y-20 transition-all group-hover:translate-y-0 text-2xl"></ion-icon>
                 <p class="-translate-y-2 group-hover:translate-y-0">Grafivos</p>
             </a>
             <a class="group py-3 px-4 inline-flex flex-col items-center gap-2 hover:bg-white hover:scale-105 transition-all text-sm text-gray-700 font-medium rounded-md shadow-lg dark:bg-gray-800 dark:text-gray-400 w-32 justify-center" href="/datosAlmacenados">
-                <ion-icon name="calendar"  class="-translate-y-20 transition-all group-hover:translate-y-0"></ion-icon>
+                <ion-icon name="calendar"  class="-translate-y-20 transition-all group-hover:translate-y-0 text-2xl"></ion-icon>
                 <p class="-translate-y-2 group-hover:translate-y-0">Diario</p>
             </a>
         </nav>
