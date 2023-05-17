f5c749773eb44c474afa89b0ed59f0e61c914bd5
unknown <ruben@gaeinformatica.com>
2023-05-17 19:01:39 +0200

    Modificando estilos de pantalla

--- resources/views/livewire/menu-diario.blade.php
+++ resources/views/livewire/menu-diario.blade.php
@@ -48,7 +48,7 @@
         @if($productos!=null)
             @foreach($productos as $producto)
                 <div id="{{$producto['nombre']}}"
-                     class="w-38 shadow-lg border-dashed border-gray-200 border-2 flex my-4 items-center gap-x-3.5 bg-white m-5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 cursor-pointer"
+                     class="w-38 shadow-lg border border-gray-200 border-2 flex my-4 items-center gap-x-3.5 bg-white m-5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 cursor-pointer"
                      draggable="true"
                      ondragstart="drag(event)">
                     <div class="flex justify-between">
@@ -62,7 +62,7 @@
     <!--Menú del día-->
     <div class="bg-blue-50 col-span-9 flex shadow-2xl rounded-xl h-5/6 w-5/6 mt-20 ml-8 justify-center flex-col px-4 py-8 shadow-lg mb-8">
 
-        <div class="w-3/4 m-auto border-2 border-dashed rounded-lg border-gray-400 p-5">
+        <div class="w-3/4 m-auto border-2 border rounded-lg border-gray-400 p-5">
             <h1 class="text-center text-3xl italic mt-4 mb-4">Menú del día</h1>
             <h4 class="text-center">Entrantes</h4>
             <div id="Entrantes" class="flex-col flex sm:flex-row rounded-xl justify-center bg-gray-300 shadow-inner shadow-blue-50 w-full mb-8 p-2 min-h-fit"
@@ -71,7 +71,7 @@
                     @foreach ($menu as $m)
                         @if ($m->plato=="Entrantes")
                             <div id="{{$m->nombre}}"
-                                 class="w-full shadow-lg border-dashed border-gray-200 border-2 flex my-4 items-center gap-x-3.5 bg-white m-5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 cursor-pointer"
+                                 class="w-full shadow-lg border border-gray-200 border-2 flex my-4 items-center gap-x-3.5 bg-white m-5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 cursor-pointer"
                                  draggable="true"
                                  ondragstart="drag(event)">
                                 <h4>{{$m->nombre}}</h4>
