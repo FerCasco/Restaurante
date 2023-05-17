14c8d599c0d9e67c3438fedc27d43ad7e466df72
unknown <ruben@gaeinformatica.com>
2023-05-17 11:08:12 +0200

    Cambiado un elemento del main menu(salas)

--- resources/views/livewire/sala.blade.php
+++ resources/views/livewire/sala.blade.php
@@ -1,10 +1,16 @@
-<aside class="absolute shadow-2xl left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 " aria-label="Sidebar">
+<aside class="absolute shadow-2xl left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 "
+       aria-label="Sidebar">
     <div class="h-full pt-8 px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
             @foreach($salas as $sala)
-                <li class="border w-full text-center border-gray-500 rounded-md p-6 mr-6 inline-block">
-                    <button wire:click="$emit('enviarSalaId',{{$sala->id}})" type="button">{{$sala->nombre}}</button>
-                </li>
+
+                <button wire:click="$emit('enviarSalaId',{{$sala->id}})" type="button" class=" w-full flex items-center justify-center h-36 border border-gray-2 shadow-lg py-2 hover:color-gray-400 bg-white px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300">
+                    <li>
+                        {{$sala->nombre}}
+                    </li>
+
+                </button>
+
             @endforeach
         </ul>
     </div>
