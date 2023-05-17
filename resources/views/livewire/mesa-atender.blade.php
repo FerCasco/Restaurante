7aa89407f6df46b5c6b38d939f182768e61e8941
unknown <ruben@gaeinformatica.com>
2023-05-17 17:03:49 +0200

    Modificaciones a revisar de estilos generales de la app

--- resources/views/livewire/mesa-atender.blade.php
+++ resources/views/livewire/mesa-atender.blade.php
@@ -2,7 +2,7 @@
 
 <div class="grid grid-cols-3 gap-4 ml-72 mt-12 pl-28">
     @foreach($familias as $familia)
-        <a class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <a class="hover:shadow-inner cursor-pointer shadow-xlblock max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$familia->nombre}}</h5>
             <p class="font-normal text-gray-700 dark:text-gray-400">Prueba</p>
         </a>
