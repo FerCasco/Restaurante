a7f33c351ad88cbb8cac14610fe42de84f111d8e
mariaperezmartin <96045981+mariaperezmartin@users.noreply.github.com>
2023-05-18 13:25:02 +0200

    "Incorporación de gráfica de stock"

--- resources/views/livewire/graficas.blade.php
+++ resources/views/livewire/graficas.blade.php
@@ -5,7 +5,17 @@
     <div class="cajaGrafico">
         <div id="precioProduct" class="w-3/4 h-full"></div>
     </div>
+
+    <div class="mt-16">
+        <button id="btnDrill"  type="button" class="bg-red-900 text-white rounded-md px-4 py-2">Drill</button>
+    </div>
+    <div class="cajaGrafico">
+        <div id="container" class="w-full h-full"></div>
+    </div>
+    
 </div>
+
+
 
 <script>
     var btngrafica = document.getElementById('btnBD');
@@ -67,7 +77,7 @@
                 headerFormat: "<span class='text-sm'> {series.name}</span><br>",
                 pointFormat: "<span style='color:{point.color}'>{point.name}</span>: <b>{point.y:.2f}</b>",
                 style: {
-                    backgroundColor: '#fff',
+                    backgroundColor: '#purple',
                     borderColor: '#000',
                     fontSize: '0.875rem',
                     padding: '0.5rem',
@@ -80,4 +90,79 @@
             }]
         }
     }
+
+
+    /***********************/
+
+    var btngrafica2 = document.getElementById('btnDrill');
+    btngrafica2.addEventListener('click', function () {
+        peticionDrill();
+    });
+
+    function peticionDrill(){
+        $.ajax({
+            url: '{{ route("mercancias") }}',
+            type: 'GET',
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+                drilldown(response)
+            },
+            error: function () {
+                alert('No conseguido');
+            },
+            contentType: 'application/json'
+        });
+    }
+
+    function drilldown(response) {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Gráficos de columnas con profundidad'
+        },
+        xAxis: {
+            type: 'category'
+        },
+        yAxis: {
+            title: {
+                text: 'Cantidad de modelos por Marca'
+            },
+        },
+        series: [{
+            name: 'Cantidad actual',
+            data: response.map(function (mercancia) {
+                return {
+                    name: mercancia.nombre,
+                    y: parseFloat(mercancia.cantidadActual),
+                    color: '#B26EFF',
+                    drilldown: mercancia.nombre
+                };
+            })
+        }],
+        drilldown: {
+            series: response.map(function (mercancia) {
+                return {
+                    id: mercancia.nombre,
+                    data: [
+                        ['Stock mínimo', parseFloat(mercancia.stockMin)],
+                        ['Hacer encargo', (((parseFloat(mercancia.stockMin)+parseFloat(mercancia.stockMax))/2)/2)],
+                        ['Equilibrio', ((parseFloat(mercancia.stockMin)+parseFloat(mercancia.stockMax))/2)],
+                        ['Stock máximo', parseFloat(mercancia.stockMax)],
+                    ].map(function (datos) {
+                        return {
+                            name: datos[0],
+                            y: datos[1]
+                        };
+                    })
+                };
+            })
+        }
+    });
+}
+    
+    
+    
 </script>
